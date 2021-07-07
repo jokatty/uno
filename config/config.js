a606df0259ab1d8987dbba23bd818b2a5036b63d
jokatty <jyotikattani@gmail.com>
2021-07-07 20:50:55 +0800

    install pg and change config file

--- config/config.js
+++ config/config.js
@@ -1,17 +1,38 @@
+// module.exports = {
+//   development: {
+//     username: 'jyotikattani',
+//     password: null,
+//     // Use "_development" suffix to indicate DB is for development purposes
+//     database: 'uno_development',
+//     host: '127.0.0.1',
+//     dialect: 'postgres',
+//   },
+//   production: {
+//     username: 'jyotikattani',
+//     password: null,
+//     database: 'uno_production',
+//     host: '127.0.0.1',
+//     dialect: 'postgres',
+//   },
+// };
+
 module.exports = {
   development: {
     username: 'jyotikattani',
     password: null,
-    // Use "_development" suffix to indicate DB is for development purposes
     database: 'uno_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
-    username: 'jyotikattani',
-    password: null,
-    database: 'uno_production',
-    host: '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: { // https://github.com/sequelize/sequelize/issues/12083
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
